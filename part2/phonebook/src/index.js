053bb0653974288057c1b88f5017470a6adc5fa1
Pedro Enrique Andrade <pefandrade@hotmail.com>
2019-07-13 21:02:06 -0300

    2b 2.10

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,47 +1,99 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', date: "2019-07-01T00:00:00.334Z", id: 1 }
-  ])
-  const [ newName, setNewName ] = useState('write here...')
-
-  const rows = () => persons.map(person =>
-    <div key={person.id}>
-      {person.name}
-    </div>
-  )
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      date: new Date().toISOString(),
-      id: persons.length + 1,
-    }
-
-    setPersons(persons.concat(personObject))
-    setNewName('')
-}
-
-  const handlePersonAdded = (event) => {
-    setNewName(event.target.value)
+const Filter = (props) => {
+  
+  const handleFilterChange = (event) => {
+    props.set(event.target.value)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      filter shown with: <input value={props.new} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+
+  const handleNameChange = (event) => {
+    props.setNa(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    props.setNo(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: props.newNa,
+      number: props.newNo,
+      date: new Date().toISOString(),
+      id: props.newPe.length + 1,
+    }
+    let found = props.newPe.find(function(element) {
+      return (element.name === props.newNa)
+      });
+    if (found === undefined) {
+      props.setPe(props.newPe.concat(personObject))
+      props.setNa('')
+      props.setNo('')
+    }
+    else {
+      alert(props.newNa + ' is already added to phonebook')
+      props.setNa('')
+      props.setNo('')
+    }
+  }
+
+  return (
+    <div>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonAdded} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={props.newNa} onChange={handleNameChange} /></div>
+        <div>number: <input value={props.newNo} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
       </form>
-      <h2>Numbers</h2>
+    </div>
+  )
+}
+
+const Persons = (props) => {
+
+  function filterItems(query) {
+    return props.persons.filter(function(el) {
+        return el.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    })
+  }
+
+  const rows = () => filterItems(props.newFi).map(person =>
+    <div key={person.id}>
+      {person.name} {person.number}
+    </div>
+  )
+
+  return (
+    <div>
+      <br/>
       {rows()}
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '99632-1057', date: "2019-07-01T00:00:00.334Z", id: 1 }
+  ])
+  const [ newName, setNewName ] = useState('write name here...')
+  const [ newNumber, setNewNumber ] = useState('write number here...')
+  const [ newFilter, setNewFilter ] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter set={setNewFilter} new={newFilter} />
+      <h3>add a new</h3>
+      <PersonForm setNo={setNewNumber} newNo={newNumber} setNa={setNewName} newNa={newName} newPe={persons} setPe={setPersons} />
+      <Persons persons={persons} newFi={newFilter} />
     </div>
   )
 }
